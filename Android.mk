b511d02796cda89c2ed5265c1203064ec2d533de
Gabriele <gabboxl0@gmail.com>
2020-09-20 16:11:15 +0200

    msm8916-common: Move keymaster to firmware/
    Change-Id: Ibca9195ed25f47e93c248f8049668a6af82fc4c5

--- Android.mk
+++ Android.mk
@@ -48,12 +48,12 @@ ALL_DEFAULT_INSTALLED_MODULES += $(ISDB_SYMLINKS)
 KM_IMAGES := \
 	keymaster.b00 keymaster.b01 keymaster.b02 keymaster.b03 keymaster.mdt
 
-KM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/keymaster/,$(notdir $(KM_IMAGES)))
+KM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(KM_IMAGES)))
 $(KM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Keymaster firmware link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /firmware/image/keymaste$(suffix $@) $@
+	$(hide) ln -sf /firmware/image/$(suffix $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(KM_SYMLINKS)
 
